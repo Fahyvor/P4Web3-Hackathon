0558c659713453d587bb79978aebf4bc17c393b4
Fahyvor <favourokafor30@gmail.com>
2023-08-07 13:56:58 +0100

    Sapphire installation

--- ERC20.sol
+++ ERC20.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 import "./IERC20.sol";
 import "./IERC20Metadata.sol";
 import "./Context.sol";
+import "@oasisprotocol/sapphire-contracts/contracts/Sapphire.sol";
 
 
 contract ERC20 is Context, IERC20, IERC20Metadata {
